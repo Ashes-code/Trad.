aacfc5cc9aa94ceae617938253cd4735812c496e
Wisdom Ezeh <wisdomcezeh@gmail.com>
2024-08-29 13:16:45 +0100

    trad project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./clients.html", "./candidates.html", "./consultants.html", "./productivity.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
